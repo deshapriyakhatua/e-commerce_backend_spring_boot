252e851e1a74bb2f3bb61091ffb2fbd9381b0936
deshapriya <deshapriyakhatua@gmail.com>
2023-09-30 14:13:57 +0530

    spring project created

---
+++ src/main/java/com/spring/rest/controler/MyController.java
@@ -0,0 +1,13 @@
+package com.spring.rest.controler;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+	@GetMapping("/home")
+	public String home() {
+		return "welcome to Spring Home Page";
+	}
+}
